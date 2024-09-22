a9ed941eeab8328ad7dfc9e71fc7dd239f018682
jkweber236 <jkweber236@gmail.com>
2024-09-21 20:16:31 -0600

    Added routes and controllers to clean up code

---
+++ controllers/lesson1.js
@@ -0,0 +1,13 @@
+const dalanRoute = (req, res) => {
+    res.send("Dalan Weber");
+};
+
+const lilyRoute = (req, res) => {
+    res.send("Lily Weber");
+};
+
+module.exports = {
+    dalanRoute,
+    lilyRoute
+};
+
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
+
+const port = 3000;
+
+app.use('/', require('./routes'));
  
-app.get('/', (req, res) => {
-  res.send("Jennifer Weber");
-});
- 
-app.listen(process.env.PORT || 3000);
-console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+app.listen(process.env.port || port);
+console.log('Web Server is listening at port ' + (process.env.port || port));
