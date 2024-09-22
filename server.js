35cc311fe9bc86a3d3aaec31c787fe27371e7be0
jkweber236 <jkweber236@gmail.com>
2024-09-21 19:36:59 -0600

    Adding lesson 1 assignment code

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send("Jennifer Weber");
+});
+ 
+app.listen(process.env.PORT || 3000);
+console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
