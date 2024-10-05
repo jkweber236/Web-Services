56eeb40a1c5dbef9f076f4b105b2ad37493f1cd4
jkweber236 <jkweber236@gmail.com>
2024-10-05 00:06:32 -0600

    Added post (create) and put (update) methods

--- Week2/W02PersonalAssignment/controllers/contacts.js
+++ Week2/W02PersonalAssignment/controllers/contacts.js
@@ -22,4 +22,38 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db('CSE341').collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(201).json(response);
+  } else {
+    res.status(500).json(response.error || 'Unable to create contact.');
+  }
+};
+
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id)
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db('CSE341').collection('contacts').replaceOne({_id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Unable to update contact.');
+  }
+};
+
+module.exports = { getAll, getSingle, createContact , updateContact};
